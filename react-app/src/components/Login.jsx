591540faeb8c651f25e0ee9b71ed5783d4b188bb
zhin abubakr <zheen99abubakr@gmail.com>
2022-07-25 15:27:49 +0300

    Feat: create Login component

---
+++ react-app/src/components/Login.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Typography,
+  Container,
+  Box,
+  Link,
+  TextField,
+  Button,
+} from "@mui/material";
+
+export default function Login() {
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <Box component="form" onValidat sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              name="password"
+              autoComplete="password"
+              autoFocus
+            />
+
+            <Link href="/signup" variant="body2" fullWidth>
+              Don't have an account? Sign Up
+            </Link>
+
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
+}
