61b4e089f74c25b77b03488d07c55ff82c542b1d
zhin abubakr <zheen99abubakr@gmail.com>
2022-08-02 09:12:59 +0300

    create user profile component to see the user information

--- react-app/src/components/UserProfile.jsx
+++ react-app/src/components/UserProfile.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import axios from "../axios";
+import { base } from "../api";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signIn } from "../store/auth";
 
-function UserProfile() {
+const UserProfile = () => {
+  const [admin, setAdmin] = useState([]);
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios({
+        method: "get",
+        url: base + "/auth/get-user-info",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjE5NTE1MzZ9.PFcypt2fLglYT-xunOtBVKrmu8xFdl7yxbpVUcjkBo4`,
+        },
+      });
+      dispatch(signIn(data.data.attributes));
+      setAdmin(data);
+      console.log(admin?.data?.attributes);
+    } catch (errro) {
+      console.log("not successful");
+    }
+  };
+
+  const userInfo = auth?.user;
+
+  console.log(userInfo?.id);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <>
+      <Container sx={{ marginTop: 10, marginLeft: 2 }}>
+        <Paper variant="outlined" sx={{ p: 4 }}>
+          <Grid container sx={{ p: 4 }}>
+            <Grid item xs={12} sx={{ display: "flex" }}>
+              <Typography variant="h4">{userInfo?.first_name}</Typography>
+            </Grid>
+            <Grid item xs={12} sx={{ p: 4 }}>
+              <Typography>ID : {userInfo?.id}</Typography>
+              <Typography>First Name : {userInfo?.first_name}</Typography>
+              <Typography>Last Name : {userInfo?.last_name}</Typography>
+              <Typography>Email : {userInfo?.email}</Typography>
+              <Typography>Role : {userInfo?.role_ids[0]}</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </>
+  );
+};
 
-export default UserProfile
-
+export default UserProfile;
