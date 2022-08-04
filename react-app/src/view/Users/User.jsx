154becc9c215d20fb969e1dbb240646156ed0d01
zhin abubakr <zheen99abubakr@gmail.com>
2022-08-04 08:52:30 +0300

    delete a user

--- react-app/src/view/Users/User.jsx
+++ react-app/src/view/Users/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../axios";
 import { base } from "../../api";
@@ -9,12 +9,47 @@ import {
   Typography,
   Button,
   Stack,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const User = () => {
   const { id } = useParams();
   const [singleUser, setSingleUser] = useState([]);
+  const [roleList, setRoleList] = useState([]);
+  const [choosedRole, setChoosedRole] = useState([]);
+  const navigate = useNavigate();
+
+  function handleChange(event) {
+    // handle change for select input (role)
+    console.log(event.target);
+    setChoosedRole(event.target.value);
+    console.log([...choosedRole] + "kboeihgpwrjpf");
+  }
+
+  function handleFunc() {
+    deleteUser();
+    navigate("/users");
+  }
+
+  //get the roles from api
+  const getRoles = async () => {
+    try {
+      const { data } = await axiosInstance({
+        method: "get",
+        url: base + "/roles",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjIxMTc3Njd9.ZqVtcTiODMehfaq_9UtXVlM88ubL7dK1ZFqS1E6imBI`,
+        },
+      });
+      // console.log(data?.data?.attributes);
+      setRoleList(data?.data);
+    } catch (errro) {
+      console.log("not successful");
+    }
+  };
 
   const getSingleUser = async () => {
     try {
@@ -32,10 +67,63 @@ const User = () => {
     }
   };
 
+  const getListOfUsers = async () => {
+    try {
+      const { data } = await axiosInstance({
+        method: "get",
+        url: base + "/users",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjE5NTE1MzZ9.PFcypt2fLglYT-xunOtBVKrmu8xFdl7yxbpVUcjkBo4`,
+        },
+      });
+      // console.log(data?.data);
+      // setListOfUser(data?.data);
+    } catch (errro) {
+      console.log("not successful");
+    }
+  };
+
+  const deleteUser = async () => {
+    try {
+      const { data } = await axiosInstance({
+        method: "delete",
+        url: base + `/users/` + id,
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjE5NTE1MzZ9.PFcypt2fLglYT-xunOtBVKrmu8xFdl7yxbpVUcjkBo4`,
+        },
+      });
+      getListOfUsers();
+      console.log(data?.data?.attributes);
+    } catch (errro) {
+      console.log("not successful");
+    }
+  };
+
+  const updateUser = async () => {
+    try {
+      const { data } = await axiosInstance({
+        method: "put",
+        url: base + `/users/` + 2,
+        body: {
+          user: {
+            role_ids: [3],
+          },
+        },
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjE5NTE1MzZ9.PFcypt2fLglYT-xunOtBVKrmu8xFdl7yxbpVUcjkBo4`,
+        },
+      });
+      console.log(data?.data?.attributes);
+    } catch (errro) {
+      console.log("not successful");
+    }
+  };
+
   useEffect(() => {
     getSingleUser();
+    getRoles();
   }, []);
-
+  // console.log(roleList[0]?.attributes?.id + "roleeeeeeeeeee");
   return (
     <>
       <Container sx={{ marginTop: 10, marginLeft: 2 }}>
@@ -53,10 +141,29 @@ const User = () => {
               <Typography>Role : {singleUser.role_ids}</Typography>
             </Grid>
           </Grid>
+          <Grid item xs={12} sx={{ p: 4 }}>
+            <InputLabel id="demo-simple-select-label">Role</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={choosedRole}
+              label="role"
+              onChange={handleChange}
+              fullWidth
+            >
+              {roleList?.map((role, index) => (
+                <MenuItem key={index} value={role?.attributes?.name}>
+                  {role?.attributes?.id} : {role?.attributes?.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
           <Grid item xs={12}>
             <Stack spacing={2} direction="row" justifyContent="end">
-              <Button variant="contained">DELETE</Button>
-              <Button variant="contained">EDIT</Button>
+              <Button variant="contained" onClick={handleFunc}>
+                DELETE
+              </Button>
+              <Button variant="contained">UPDATE</Button>
             </Stack>
           </Grid>
         </Paper>
