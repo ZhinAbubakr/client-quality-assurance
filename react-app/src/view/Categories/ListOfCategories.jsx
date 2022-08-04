01cf97a7e6276551914606ed2665fb627083c97d
zhin abubakr <zheen99abubakr@gmail.com>
2022-08-04 08:51:36 +0300

    get list of categories from api

--- react-app/src/view/Categories/ListOfCategories.jsx
+++ react-app/src/view/Categories/ListOfCategories.jsx
@@ -7,64 +7,70 @@ import {
   ListItemIcon,
   ListItemText,
   Stack,
+  Typography,
 } from "@mui/material";
 import { Container } from "@mui/system";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosInstance from "../../axios";
+import { base } from "../../api";
 
 const ListOfCategories = () => {
-  const [checked, setChecked] = React.useState([0]);
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    // const newChecked = [...checked];
+  // const handleToggle = (value) => () => {
+  //   const currentIndex = checked.indexOf(value);
+  // const newChecked = [...checked];
 
-    // if (currentIndex === -1) {
-    //   newChecked.push(value);
-    // } else {
-    //   newChecked.splice(currentIndex, 1);
-    // }
+  // if (currentIndex === -1) {
+  //   newChecked.push(value);
+  // } else {
+  //   newChecked.splice(currentIndex, 1);
+  // }
 
-    // setChecked(newChecked);
+  // setChecked(newChecked);
+  // };
+
+  const [listOfCategories, setListOfCategories] = useState([]);
+
+  const getCategories = async () => {
+    try {
+      const { data } = await axiosInstance({
+        method: "get",
+        url: base + "/categories",
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjE5NTE1MzZ9.PFcypt2fLglYT-xunOtBVKrmu8xFdl7yxbpVUcjkBo4`,
+        },
+      });
+      console.log(data?.data);
+      setListOfCategories(data?.data);
+    } catch (errro) {
+      console.log("not successful");
+    }
   };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   return (
     <>
       <Container sx={{ marginLeft: 5, marginTop: 12, p: 4 }}>
-        <List
-          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-        >
-          {["AI", "REACTJS", "IT", "JAVA"].map((value) => {
-            const labelId = `checkbox-list-label-${value}`;
-
-            return (
-              <ListItem>
-                <ListItemButton
-                  role={undefined}
-                  onClick={handleToggle(value)}
-                  dense
-                >
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      // checked={checked.indexOf(value) !== -1}
-                      tabIndex={-1}
-                      disableRipple
-                      inputProps={{ "aria-labelledby": labelId }}
-                    />
-                  </ListItemIcon>
-                  <ListItemText id={labelId} primary={`${value}`} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-        </List>
+        {listOfCategories.map((categories) => (
+          <List key={categories.attributes.id}>
+            <ListItem>
+              <ListItemButton role={undefined} dense>
+                <Checkbox {...label} />
+                <ListItemText
+                  id={categories.attributes.id}
+                  primary={categories.attributes.name}
+                />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        ))}
         <Stack spacing={2} direction="row" justifyContent="end">
-          <Button backgroundColor="secondary" variant="contained">
-            DELETE
-          </Button>
-          <Button backgroundColor="secondary" variant="contained">
-            EDIT
-          </Button>
+          <Button variant="contained">DELETE</Button>
+          <Button variant="contained">EDIT</Button>
         </Stack>
       </Container>
     </>
