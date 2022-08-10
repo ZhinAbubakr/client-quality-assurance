5c37379cb94727e00e9816205d18274b3eaf1a3c
zhin abubakr <zheen99abubakr@gmail.com>
2022-08-10 16:04:11 +0300

    enhance card component for questions

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -1,39 +1,65 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Chip, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getCategories } from "../view/Question/Popup";
 
 export default function Cards({ questions }) {
+  const [categoryList, setCategoryList] = useState([]);
+
   const navigate = useNavigate();
+
+  console.log("categoryList", categoryList);
+
+  useEffect(() => {
+    getCategories(setCategoryList);
+  }, []);
+
   return (
-    <>
+    <Box display="flex" flexDirection="column">
       {questions.map((ques) => (
-        <Box
-          component="span"
-          sx={{ p: 1 }}
+        <Card
           key={ques.attributes.id}
           onClick={() => {
             return navigate("/questions/" + ques.attributes.id);
           }}
+          sx={{
+            cursor: "pointer",
+            my: 2,
+          }}
         >
-          <Card
-            variant="outlined"
-            sx={{
-              p: 1,
-              cursor: "pointer",
-              "&:hover": {
-                backgroundColor: "#5c6bc0",
-                color: "#ffffff",
-              },
-            }}
-          >
-            <CardContent>
-              <Typography variant="h5" component="div">
-                {ques.name}
-              </Typography>
-              <Typography variant="body2">{ques.attributes.title}</Typography>
-            </CardContent>
-          </Card>
-        </Box>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {new Date(ques.attributes.created_at).toDateString()}
+            </Typography>
+            <Typography variant="h5" component="div">
+              {ques.attributes.title}
+            </Typography>
+            <Typography variant="body2" sx={{ py: 2 }}>
+              {ques.attributes.content}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {ques.attributes.category_ids.map((id, i) => (
+                <Chip
+                  color="primary"
+                  key={i}
+                  label={
+                    categoryList.find(
+                      (category) => category.attributes.id === id
+                    )?.attributes.name
+                  }
+                />
+              ))}
+            </Typography>
+          </CardContent>
+          {/* <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions> */}
+        </Card>
       ))}
-    </>
+    </Box>
   );
 }
