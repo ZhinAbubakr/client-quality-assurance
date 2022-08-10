703eab4a8ed2284461bb51d199a78c5709ccdc4e
zhin abubakr <zheen99abubakr@gmail.com>
2022-08-10 16:42:35 +0300

    fix: reuse table to show categories

--- react-app/src/view/Categories/ListOfCategories.jsx
+++ react-app/src/view/Categories/ListOfCategories.jsx
@@ -1,59 +1,82 @@
-import {
-  Button,
-  Checkbox,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Stack,
-} from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React, { useState, useEffect } from "react";
-import axiosInstance from "../../axios";
-import { base } from "../../api";
+// import axiosInstance from "../../axios";
+// import { base } from "../../api";
+import Table from "../../components/Table";
+import { useNavigate } from "react-router-dom";
+import PopupCreate from "./PopupCreate";
+import { getCategories } from "../../server";
 
 const ListOfCategories = () => {
-  const label = { inputProps: { "aria-label": "Checkbox demo" } };
-
   const [listOfCategories, setListOfCategories] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
 
-  const getCategories = async () => {
-    try {
-      const { data } = await axiosInstance({
-        method: "get",
-        url: base + "/categories",
-      });
-      console.log(data?.data);
-      setListOfCategories(data?.data);
-    } catch (errro) {
-      console.log("not successful");
-    }
+  const [cols] = useState([
+    {
+      name: "id",
+      label: "ID",
+    },
+    {
+      name: "name",
+      label: "Name",
+    },
+    {
+      action: "action",
+      label: "Action",
+    },
+  ]);
+
+  const navigate = useNavigate();
+
+  // const getCategories = async () => {
+  //   try {
+  //     const { data } = await axiosInstance({
+  //       method: "get",
+  //       url: base + "/categories",
+  //     });
+  //     console.log(data?.data);
+  //   } catch (errro) {
+  //     console.log("not successful");
+  //   }
+  // };
+
+  const test = async () => {
+    const data = await getCategories();
+    console.log(data.data);
+    setListOfCategories(data?.data);
   };
 
   useEffect(() => {
-    getCategories();
+    test();
   }, []);
+
+  const SelectedRow = (id) => {
+    navigate("/category/" + id);
+    console.log("go to category page");
+  };
 
   return (
     <>
       <Container sx={{ marginLeft: 5, marginTop: 12, p: 4 }}>
-        {listOfCategories.map((categories) => (
-          <List key={categories.attributes.id}>
-            <ListItem>
-              <ListItemButton role={undefined} dense>
-                <Checkbox {...label} />
-                <ListItemText
-                  id={categories.attributes.id}
-                  primary={categories.attributes.name}
-                />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        ))}
-        <Stack spacing={2} direction="row" justifyContent="end">
-          <Button variant="contained">DELETE</Button>
-          <Button variant="contained">EDIT</Button>
-        </Stack>
+        <Typography variant="h5">List of Categories</Typography>
+        <Button
+          sx={{ float: "right" }}
+          variant="contained"
+          onClick={() => {
+            setOpenPopup(true);
+            console.log("add question");
+            //cally aw functiona bkawa ka list of questiont nishandadat!
+          }}
+        >
+          ADD
+        </Button>
+        <Table users={listOfCategories} cols={cols} SelectedRow={SelectedRow} />
+        <PopupCreate
+          openPopup={openPopup}
+          setOpenPopup={setOpenPopup}
+          getCategories={test}
+        />
       </Container>
     </>
   );
